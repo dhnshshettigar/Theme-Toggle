cc9d160efb9ddee343036b890b45d4200efe26d8
dhnshshettigar <dhnshshettigar@gmail.com>
2025-07-03 19:56:20 +0530

    body color also changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {FaSun, FaMoon} from 'react-icons/fa'
 import {IconContext} from "react-icons";
-import {useState} from "react";
 
 const Header = () => {
     const lightTheme = {
@@ -14,17 +13,19 @@ const Header = () => {
     }
     const [lightMode, setLightMode] = useState(true);
     const [theme, setTheme] = useState(lightTheme);
+
+    useEffect(() => {
+        document.body.style.backgroundColor = lightMode ? lightTheme.backgroundColor : darkTheme.backgroundColor;
+        document.body.style.color = lightMode ? lightTheme.color : darkTheme.color;
+    },[lightMode])
+
     return (
         <div className="flex justify-between items-center p-5 " style={theme}>
             <h1 className="text-3xl" >ThemeToggle</h1>
             <IconContext.Provider value={{ className: "text-2xl" }}>
                 <button onClick={
                     () => {
-                        if (lightMode) {
-                            setTheme(darkTheme);
-                        } else {
-                            setTheme(lightTheme);
-                        }
+                        setTheme(lightMode ? darkTheme : lightTheme);
                         setLightMode(!lightMode);
                     }
                 }>{ lightMode ? <FaMoon /> : <FaSun/>}</button>
